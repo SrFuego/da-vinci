6344adc7bfcf1e8419df8a86a7e3a1a6d34de6e1
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2023-07-22 03:01:24 -0500

    WIP run fake tests in spanish in behave over Python

---
+++ src/features/steps/primer_contacto.py
@@ -0,0 +1,16 @@
+from behave import *
+
+
+@given("una persona en un navegador web")
+def step_impl(context):
+    pass
+
+
+@when("abre davinci.srfuego.dev")
+def step_impl(context):
+    assert True is not False
+
+
+@then('le muestra "{contenido}"')
+def step_impl(context, contenido):
+    assert context.failed is False
