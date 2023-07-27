b3763b8b36ad5e9e86e2260dd214f246ed2a59f2
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2023-07-27 02:47:58 -0500

    ADD ingresar pregunta .feature

---
+++ src/features/steps/ingresar_pregunta.py
@@ -0,0 +1,37 @@
+from behave import given, when, then
+
+
+@given("un administrador autenticado dentro del backoffice")
+def step_impl(context):
+    # raise NotImplementedError(
+    #     "STEP: Given un administrador autenticado dentro del backoffice"
+    # )
+    pass
+
+
+@when('ingresa "{pregunta}"')
+def step_impl(context, pregunta):
+    # raise NotImplementedError('STEP: When ingresa "Cuanto es 2+2?"')
+    pass
+
+
+@when('agrega "{cantidad_de_alternativas}"')
+def step_impl(context, cantidad_de_alternativas):
+    # raise NotImplementedError('STEP: When agrega "{pregunta_1}"')
+    pass
+
+
+@when('agrega "{alternativas}" la cantidad de veces ingresada')
+def step_impl(context, alternativas):
+    # raise NotImplementedError(
+    #     'STEP: When agrega "2,3,4" la cantidad de veces ingresada'
+    # )
+    pass
+
+
+@then("se muestra en la App para los usuarios")
+def step_impl(context):
+    # raise NotImplementedError(
+    #     "STEP: Then se muestra en la App para los usuarios"
+    # )
+    pass
