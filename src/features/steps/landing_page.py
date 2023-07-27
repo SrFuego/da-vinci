ead57ab893ac63ba04d861f3e1ea850c4b994a5f
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2023-07-26 21:07:59 -0500

    ADD mostrar preguntas .feature

--- src/features/steps/landing_page.py
+++ src/features/steps/landing_page.py
@@ -1,37 +1,46 @@
-from behave import *
+from behave import given, when, then
 
 # Esquema del escenario: Página web
 
+
 @given("un humano en un navegador web")
 def step_impl(context):
+    # raise NotImplementedError("STEP: Given un humano en un navegador web")
     pass
 
 
 @when("abre davinci.srfuego.dev")
 def step_impl(context):
-    assert True is not False
+    # raise NotImplementedError("STEP: When abre davinci.srfuego.dev")
+    pass
 
 
 @then('puede ver "{contenido}"')
 def step_impl(context, contenido):
-    assert context.failed is False
+    # raise NotImplementedError('STEP: Then puede ver "{contenido}"')
+    pass
 
 
 # Esquema del escenario: Descargar Da Vinci
 
-@given(u'un humano en la landing page de Da Vinci')
+
+@given("un humano en la landing page de Da Vinci")
 def step_impl(context):
-    # raise NotImplementedError(u'STEP: Given un humano en la landing page de Da Vinci')
+    # raise NotImplementedError("STEP: Given un humano en la landing page de Da Vinci")
     pass
 
 
-@when(u'hago clic en el botón de descarga para "{sistema_operativo}"')
+@when('hago clic en el botón de descarga para "{sistema_operativo}"')
 def step_impl(context, sistema_operativo):
-    # raise NotImplementedError(u'STEP: When hago clic en el botón de descarga para "Android"')
-    assert True is not False
+    # raise NotImplementedError(
+    #     'STEP: When hago clic en el botón de descarga para "{sistema_operativo}"'
+    # )
+    pass
 
 
-@then(u'es redirigido a la página de descarga de la App en "{store}"')
+@then('es redirigido a la página de descarga de la App en "{store}"')
 def step_impl(context, store):
-    # raise NotImplementedError(u'STEP: Then soy redirigido a la página de descarga de la App en "Google Play"')
-    assert context.failed is False
+    # raise NotImplementedError(
+    #     'STEP: Then es redirigido a la página de descarga de la App en "{store}"'
+    # )
+    pass
