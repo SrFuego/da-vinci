4c1c7cb56945da0f96279c8b9639ff7b2770ddd0
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2023-08-28 19:31:21 -0500

    FIX clean code

--- src/app/lib/domain/bloc/pregunta.dart
+++ src/app/lib/domain/bloc/pregunta.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
 import '../models/pregunta.dart';
 import '../models/solucion.dart';
 import '../../infrastructure/repository/pregunta.dart';
 
-class PreguntaBloc extends ChangeNotifier {
+class PreguntaBloc {
+// class PreguntaBloc extends ChangeNotifier {
   final repository = PreguntaRepositoryImpl();
 
   Future<Pregunta> getPreguntaAleatoria() {
