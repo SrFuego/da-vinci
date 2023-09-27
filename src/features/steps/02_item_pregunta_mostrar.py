ba144244ccb54a8c6eaad6033ac2d6ac0ae41fb2
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2023-09-27 12:30:23 -0500

    ADD feature and step for pregunta individual por curso

--- src/features/steps/02_item_pregunta_mostrar.py
+++ src/features/steps/02_item_pregunta_mostrar.py
@@ -20,8 +20,6 @@ Curso = apps.get_model("core", "Curso")
 Tema = apps.get_model("core", "Tema")
 # Solucion = apps.get_model("core", "Solucion")
 
-# Flujo Pregunta Aleatoria
-
 
 @given("un usuario en el home de la App Da Vinci")
 def usuario_en_el_home(context):
@@ -38,16 +36,17 @@ def pantalla_de_opciones(context):
     pass
 
 
+# Flujo Pregunta Individual Aleatoria
+
+
 @when('selecciona "Pregunta Aleatoria"')
 def selecciona_pregunta_aleatoria(context):
     pass
 
 
-@then("le muestra un problema de admisión y sus alternativas")
-def muestra_problema_de_admision(context):
-    response = context.test.client.get(
-        "http://localhost:8000/api/v1/pregunta_aleatoria/"
-    )
+@then("le muestra un problema aleatorio y sus alternativas")
+def muestra_problema_aleatorio_de_admision(context):
+    response = context.test.client.get(context.api_localhost + "pregunta_aleatoria/")
     context.test.assertEqual(response.status_code, 200)
     context.test.assertIn("enunciado", response.data)
     context.test.assertIn("alternativas", response.data)
@@ -77,3 +76,60 @@ def step_impl(context):
     # )
     # TODO: Mostrar examen de admisión
     pass
+
+
+# Flujo Pregunta Individual por Curso
+
+
+@when('que selecciona "Elegir Curso"')
+def selecciona_elegir_curso(context):
+    pass
+
+
+@when("le muestra la lista de Cursos")
+def muestra_lista_cursos(context):
+    response = context.test.client.get(context.api_localhost + "curso/")
+    context.test.assertEqual(response.status_code, 200)
+    context.test.assertIn("cursos", response.data)
+
+
+@when("selecciona un Curso")
+def selecciona_un_curso(context):
+    pass
+
+
+@then("le muestra un problema del curso y sus alternativas")
+def muestra_problema_del_curso(context):
+    curso_seleccionado_id = 1
+    response = context.test.client.get(
+        context.api_localhost + "pregunta_individual/?curso={curso_seleccionado_id}"
+    )
+    context.test.assertEqual(response.status_code, 200)
+    context.test.assertIn("enunciado", response.data)
+    context.test.assertIn("alternativas", response.data)
+    curso_seleccionado = Curso.objects.get(id=curso_seleccionado_id)
+    context.curso_seleccionado = curso_seleccionado
+    context.response_data = response.data
+
+
+# @when('le da al botón: "Enviar"')
+# def step_impl(context):
+#     # raise NotImplementedError('STEP: When le da al botón: "Enviar"')
+#     pass
+
+
+@then("el Curso (el que seleccionó)")
+def muestra_el_curso_seleccionado(context):
+    context.test.assertIn("curso", context.response_data)
+    context.test.assertTrue(len(context.response_data["curso"]) > 0)
+    context.test.assertEquals(
+        context.curso_seleccionado.nombre, context.response_data["curso"]["nombre"]
+    )
+
+
+@then("el Tema (cualquiera del Curso seleccionado)")
+def muestra_el_tema_y_es_del_curso(context):
+    context.test.assertIn("tema", context.response_data)
+    context.test.assertTrue(len(context.response_data["tema"]) > 0)
+    tema_response = Tema.objects.get(nombre=context.response_data["tema"]["nombre"])
+    context.test.assertIn(tema_response, context.curso_seleccionado.tema_set.all())
