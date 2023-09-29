a5a9ce743d66120b04e92fe4d1a35d46af6d747b
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2023-09-29 02:12:22 -0500

    ADD flutter improve curso code

---
+++ src/app/lib/infrastructure/repository/curso.dart
@@ -0,0 +1,16 @@
+import '../../domain/models/curso.dart';
+import '../../domain/repository/curso.dart';
+import '../../infrastructure/repository/dio_client.dart';
+
+class CursoRepositoryImpl implements CursoRepository {
+  @override
+  Future<List<Curso>> getCursos() async {
+    List<Curso> cursosApi = [];
+    final data = await DioClient.instance.get('/curso/');
+    for (var index = 0; index < data['cursos'].length; index++) {
+      Curso aux = Curso.fromJson(data['cursos'][index]);
+      cursosApi.add(aux);
+    }
+    return cursosApi;
+  }
+}
