0bcab1f861e86d73e2bd52e7ff4bef0518609868
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2023-10-01 02:36:19 -0500

    WIP refactor presentation layer

--- src/app/lib/presentation/screens/navigation/pregunta_individual.dart
+++ src/app/lib/presentation/screens/navigation/pregunta_individual.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../../components/navigation_button.dart';
-import '../../components/base_screen.dart';
+import '../../components/route_button.dart';
+import '../base_screen.dart';
 
 class PreguntaIndividualScreen extends StatelessWidget {
   const PreguntaIndividualScreen({super.key});
@@ -15,12 +15,12 @@ class PreguntaIndividualScreen extends StatelessWidget {
         RouterButtonWithDescription(
             title: 'Pregunta Aleatoria',
             description: 'Muestra pregunta, califica y muestra respuesta',
-            route: '/individual/aleatoria'),
+            route: 'individual/aleatoria/'),
         SizedBox(height: 80.0),
         RouterButtonWithDescription(
             title: 'Seleccionar Curso',
             description: 'Combinados, por tema, por curso',
-            route: '/individual/curso'),
+            route: 'individual/curso/'),
       ],
     );
   }
