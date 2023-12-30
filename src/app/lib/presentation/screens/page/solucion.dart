dcfd88349931887026777ce70f6f60011763839f
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2023-12-29 20:58:05 -0500

    FIX design and include LaTeX in Pregunta and Solucion UI

--- src/app/lib/presentation/screens/page/solucion.dart
+++ src/app/lib/presentation/screens/page/solucion.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_tex/flutter_tex.dart';
+
 import '../../../domain/models/solucion.dart';
+import '../../../domain/models/pregunta.dart';
 
 import '../base_screen.dart';
 
@@ -21,55 +24,61 @@ class SolucionPage extends StatefulWidget {
 class _SolucionScreenState extends State<SolucionPage> {
   @override
   Widget build(BuildContext context) {
-    RespuestaEvaluada respuestaEvaluada = widget.respuestaEvaluada;
+    RespuestaEvaluada respuesta = widget.respuestaEvaluada;
+    Pregunta pregunta = respuesta.solucion.pregunta;
     return BaseScreen(
-      title: respuestaEvaluada.solucion.pregunta.curso.nombre,
+      title: pregunta.curso.nombre,
       body: <Widget>[
+        const SizedBox(height: 20.0),
         Center(
-          child: Text("Puntaje obtenido: ${respuestaEvaluada.puntajeObtenido}"),
+          child: Text(
+            "Puntaje obtenido: ${respuesta.puntajeObtenido}",
+          ),
         ),
         const SizedBox(height: 15.0),
         Center(
-          child: Text(respuestaEvaluada.solucion.pregunta.tema.nombre + (':')),
+          child: Text(pregunta.tema.nombre + (':')),
         ),
         const SizedBox(height: 25.0),
-        Center(
-          child: Text(respuestaEvaluada.solucion.pregunta.enunciado),
+        TeXView(
+          child: TeXViewColumn(
+            children: [
+              TeXViewDocument(
+                pregunta.enunciado,
+                style: const TeXViewStyle(
+                  textAlign: TeXViewTextAlign.center,
+                ),
+              ),
+            ],
+          ),
         ),
-        const SizedBox(height: 30.0),
+        const SizedBox(height: 7.0),
         SizedBox(
           width: 400,
           child: Column(
             children: [
-              for (var i = 0;
-                  i < respuestaEvaluada.solucion.pregunta.alternativas.length;
-                  i++)
+              for (var i = 0; i < pregunta.alternativas.length; i++)
                 Container(
-                  color: respuestaEvaluada.esCorrecta &
-                          (respuestaEvaluada
-                                  .solucion.pregunta.alternativas[i].id ==
-                              respuestaEvaluada.alternativaEnviada.id)
+                  color: respuesta.esCorrecta &
+                          (pregunta.alternativas[i].id ==
+                              respuesta.alternativaEnviada.id)
                       ? Colors.lightGreen
-                      : (respuestaEvaluada
-                                  .solucion.pregunta.alternativas[i].id ==
-                              respuestaEvaluada.alternativaEnviada.id)
+                      : (pregunta.alternativas[i].id ==
+                              respuesta.alternativaEnviada.id)
                           ? Colors.redAccent
-                          : (respuestaEvaluada
-                                      .solucion.pregunta.alternativas[i].id ==
-                                  respuestaEvaluada
-                                      .solucion.alternativaCorrecta.id)
+                          : (pregunta.alternativas[i].id ==
+                                  respuesta.solucion.alternativaCorrecta.id)
                               ? Colors.lightBlueAccent
                               : null,
                   child: RadioListTile<int>(
                     title: Text(
                       '${String.fromCharCode(i + 97)})\t'
-                      '${respuestaEvaluada.solucion.pregunta.alternativas[i].valor}',
+                      '${pregunta.alternativas[i].valor}',
                     ),
-                    value:
-                        respuestaEvaluada.solucion.pregunta.alternativas[i].id,
-                    groupValue: respuestaEvaluada.esCorrecta
-                        ? respuestaEvaluada.alternativaEnviada.id
-                        : respuestaEvaluada.solucion.alternativaCorrecta.id,
+                    value: pregunta.alternativas[i].id,
+                    groupValue: respuesta.esCorrecta
+                        ? respuesta.alternativaEnviada.id
+                        : respuesta.solucion.alternativaCorrecta.id,
                     onChanged: (int? value) {
                       // setState(() {});
                     },
@@ -84,13 +93,38 @@ class _SolucionScreenState extends State<SolucionPage> {
             child: Column(
               children: [
                 const SizedBox(height: 25.0),
-                const Text("Solución"),
-                const SizedBox(height: 25.0),
-                Text("Resolucion: ${respuestaEvaluada.solucion.resolucion}"),
-                const SizedBox(height: 15.0),
-                Text("Teoría: ${respuestaEvaluada.solucion.teoria}"),
+                const Text("Solución:"),
+                const SizedBox(height: 7.0),
+                TeXView(
+                  child: TeXViewColumn(
+                    children: [
+                      TeXViewDocument(
+                        respuesta.solucion.teoria,
+                        style: const TeXViewStyle(
+                          textAlign: TeXViewTextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 const SizedBox(height: 40.0),
+                const Text("Resolución:"),
+                const SizedBox(height: 7.0),
+                TeXView(
+                  child: TeXViewColumn(
+                    children: [
+                      TeXViewDocument(
+                        respuesta.solucion.resolucion,
+                        style: const TeXViewStyle(
+                          textAlign: TeXViewTextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 15.0),
                 widget.botonSiguientePregunta,
+                const SizedBox(height: 40.0),
               ],
             ),
           ),
