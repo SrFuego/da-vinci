814ca0da8ae3ee8e6c434fe58bac48ef7d5a534f
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2025-02-14 08:33:56 -0500

    ADD initial Swift App

---
+++ app-swift/daVinci/ExamenesPasadosView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ExamenesPasadosView: View {
+    var body: some View {
+        Text("Exámenes Pasados")
+            .navigationTitle("Exámenes Pasados")
+    }
+}
+
+struct ExamenesPasadosView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExamenesPasadosView()
+    }
+}
---
+++ app-swift/daVinci/GrupoDePreguntasView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct GrupoDePreguntasView: View {
+    var body: some View {
+        Text("Grupo de Preguntas")
+            .navigationTitle("Grupo de Preguntas")
+    }
+}
+
+struct GrupoDePreguntasView_Previews: PreviewProvider {
+    static var previews: some View {
+        GrupoDePreguntasView()
+    }
+}
