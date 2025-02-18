3af34a1c5c057460c2776521b515f041ed9babf5
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2025-02-18 00:08:23 -0500

    UPDATE requirements and tools

--- api-django/tools/requirements_update.sh
+++ api-django/tools/requirements_update.sh
@@ -9,5 +9,5 @@ environments=("develop")
 for i in ${!environments[@]}; do
     environment=${environments[$i]}
     echo "Environment: ${environment}"
-    pip-compile -r api/requirements/${environment}.in -o api/requirements/$(date +'%Y%m%d').${environment}
+    pip-compile -r api-django/requirements/${environment}.in -o api-django/requirements/$(date +'%Y%m%d').${environment}
 done
